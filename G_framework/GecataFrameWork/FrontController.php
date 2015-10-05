21fefb1a86ce452360da46d568bdbbfff575d3f8
Georgi Dimitrov <gb_dimitrov@yahoo.com>
2015-10-05 23:34:51 +0300

    FrontController selecting namespace

--- G_framework/GecataFrameWork/FrontController.php
+++ G_framework/GecataFrameWork/FrontController.php
@@ -12,6 +12,9 @@ namespace GF;
 class FrontController
 {
     private static $_instance = null;
+    private $namespace = null;
+    private $controller = null;
+    private $method = null;
 
     private function __construct()
     {
@@ -22,6 +25,24 @@ class FrontController
         $a = new \GF\Routers\DefaultRouter();
         $_uri = $a->getURI();
         $routes = \GF\App::getInstance()->getConfig()->routes;
+        if (is_array($routes) && count($routes) > 0) {
+            foreach ($routes as $key => $val) {
+                if (stripos($_uri, $key) === 0 && $val['namespace']) {
+                    $this->namespace = $val['namespace'];
+                    break;
+                }
+            }
+        } else {
+            //TODO
+            throw new \Exception('Default route missing', 500);
+        }
+        if ($this->namespace == null && $routes['*']['namespace']) {
+            $this->namespace = $routes['*']['namespace'];
+        } else if ($this->namespace == null && !$routes['*']['namespace']) {
+            //TODO
+            throw new \Exception('Default router is missing',500);
+        }
+        echo $this->namespace;
     }
 
     public function getDefaultController()
